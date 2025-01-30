e2e95a1b584592303f332e67ac8f30020c136de2
TotPro <s1290028@u-aizu.ac.jp>
2025-01-30 19:18:57 +0900

    add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local keybinds = require("keybinds")
+
+-- This table will hold the configuration.
+local config = {}
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.disable_default_key_bindings = true
+config.keys = keybinds.keys
+config.key_tables = keybinds.key_tables
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "AdventureTime"
+config.color_scheme = "Aci (Gogh)"
+-- config.color_scheme = "aikofog (terminal.sexy)"
+-- config.color_scheme = "Andromeda"
+
+-- and finally, return the configuration to wezterm
+return config
